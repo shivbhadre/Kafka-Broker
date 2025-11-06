afdb7bc763d428f15024ab26a9b726e9bc502635
unknown <shivbhadre17@gmail.com>
2025-11-06 15:28:15 +0530

    Implemented Weather Cache

---
+++ Cache_using_map_wether/src/main/java/com/example/demo/CacheUsingMapWetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheUsingMapWetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheUsingMapWetherApplication.class, args);
+	}
+
+}
